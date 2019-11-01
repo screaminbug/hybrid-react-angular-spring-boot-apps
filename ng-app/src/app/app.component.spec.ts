33157ecebe0cd581f79debda1f7f04da79ef213a
Tomislav <tstrelar@gmail.com>
2019-11-01 17:55:55 +0100

    Update ng version

--- ng-app/src/app/app.component.spec.ts
+++ ng-app/src/app/app.component.spec.ts
@@ -1,10 +1,19 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeroesComponent
+      ],
+      imports: [
+        BrowserModule,
+        FormsModule
       ],
     }).compileComponents();
   }));
@@ -13,15 +22,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Tour of Heroes'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Tour of Heroes');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
   }));
 });
