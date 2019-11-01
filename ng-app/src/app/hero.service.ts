decd8c5f78934622c834b18a58b35dac9b82e45c
Tomislav <tstrelar@gmail.com>
2019-11-01 19:59:03 +0100

    Implement heroes upto http

--- ng-app/src/app/hero.service.ts
+++ ng-app/src/app/hero.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import {Observable} from 'rxjs/internal/Observable';
+import {of} from 'rxjs/internal/observable/of';
+import {MessageService} from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  getHeroes() : Hero[] {
-    return HEROES;
+  getHeroes() : Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 
+  getHero(id: number) : Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find($ => $.id === id));
+  }
 }
